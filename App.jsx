52d586f24df13116675cac40905d2c3f374789de
EdKryp <edwincano653@gmail.com>
2024-04-17 01:10:29 -0600

    y

--- App.jsx
+++ App.jsx
@@ -3,6 +3,14 @@ import React, {useState} from 'react';
 import SearchBar from "./Components/SearchBar";
 import ProductsList from "./Components/ProductsList";
 import products from "./data";
+import { useEffect } from 'react';
+
+useEffect(() => {
+    const results = products.filter(product =>
+        product.name.toLowerCase().includes(search.toLowerCase())
+    );
+    setFilteredProducts(results);
+}, [search]);
 
 export default function App() {
   const [search, setSearch] = useState('');
@@ -10,8 +18,8 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <SearchBar value={search} onChangeText={}/>
-        <ProductsList products={filteredProducts}/>
+      <SearchBar value={search} onChangeText={(text) => setSearch(text)} />
+      <ProductsList products={filteredProducts}/>
     </View>
   );
 }
